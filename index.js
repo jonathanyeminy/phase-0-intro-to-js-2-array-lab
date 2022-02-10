be337f556b843d6a84a74f31d472052e08bcc94e
Jonathan Yeminy <jonathanyeminy@yahoo.com>
2022-02-10 18:54:01 -0500

    commit tree 6b91fca2123a02026b2eb5510b7cc4ef7058e4a8

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution here!
+
+
+let cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat () {
+    cats.push("Ralph")
+}
+
+function destructivelyPrependCat() {
+    cats.unshift("Bob")
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+}
+
+function appendCat() {
+    let newCatList = [...cats, "Broom"]
+    return newCatList
+}
+
+function prependCat() {
+    let newCatList2 = ["Arnold", ...cats,]
+    return newCatList2
+}
+
+function removeLastCat() {
+    let newCatList3 = cats.slice(0,-1)
+    return newCatList3
+}
+
+function removeFirstCat() {
+    let newCatList4 = cats.slice(1)
+    return newCatList4
+}
+
